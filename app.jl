3141700e14ab663d0900dc02b048df2e3e86b187
Harsha Veeramachaneni <hveera@gmail.com>
2024-12-08 16:08:34 -0600

    list of facts

--- app.jl
+++ app.jl
@@ -6,111 +6,94 @@ using GenieFramework
 using StippleLatex
 using StippleUI
 
-
 @genietools
 
-
-mutable struct Fact{P}
+@kwdef mutable struct Fact{P}
+    id::Int
     what_text::String
     when_text::String
     prob_range::P
     is_query::Bool
+    printable::String = ""
 end
 
+function Fact(id, what_text, when_text, prob_range, is_query)
+    f = Fact{typeof(prob_range)}(id, what_text, when_text, prob_range, is_query, "")
+    f.printable = to_string(f)
+    return f
+end
 
 @app begin
+    @private id = 1
     @in what_text = ""
     @in when_text = ""
     @in prob_range = (min=0.0, max=1.0)
-    @in prob_range_changed = false
-    @in type = ""
+    @in assume_btn_clicked = false
+    @in query_btn_clicked = false
+    @in is_query = false
 
     @in process_new_fact = false  
     @in clear_completed = false  
-    @in delete_fact = 0  
+    @in delete_fact_id = 0  
  
     # Define private and output variables
-    @private facts = Fact[]  
+    @out fact = Fact(0, "", "", (min=0.0, max=1.0), false, "")
+    @out facts = Fact[]  
     @out num_facts = 0  
 
-    @onchange type  begin
-        @info "Type changed" type
-        #@info prob_range
+    @onbutton assume_btn_clicked  begin
+        is_query = false
+        length(parse_input(what_text).vals) == 0 && return 
+
+        fact = Fact(id, what_text, when_text, prob_range, is_query)
+        id += 1
+        push!(facts, fact)
+        @push facts
+
+        num_facts = length(facts)
+        @show num_facts
+        for f in facts
+            @show f.what_text, f.when_text ,  f.prob_range , f.is_query
+        end
+    end
+
+    @onbutton query_btn_clicked  begin
+        is_query = true
+        fact = Fact(0, what_text, when_text, (min=0.0, max=1.0), is_query)
+    end
+
+    @onchange delete_fact_id begin
+        filter!(f -> f.id != delete_fact_id, facts)
+        @push facts
+        num_facts = length(facts)
     end
 
     @onchange prob_range begin
         @info "Probability range changed" prob_range
     end
 
-    @onchange prob_range_changed begin
-        @info "Probability range changed" prob_range
-    end
-
 
     @onchange facts begin
-        @info "Facts changed" todos
+        @info "Facts changed" facts
     end
 end
 
-# function ui()
-
-#     col([
-#         row([
-#             cell(    
-#                 # Header cell with app description
-#                 class="col-12", 
-#                 h1("FactSimply: What Do Your Beliefs Imply?", style="margin-bottom: 50px; text-decoration: underline;"),
-#             )
-#         ]),
-
-#         row([  
-#             textfield(class = "col-3", "What", :input, hint = "E.g., A, !B"),
-#             Stipple.Html.div(class = "col-1", col([
-#                 p("|", style="font-weight: bold;"),
-#                 p("|", style="font-weight: bold;")
-#             ])),
-#             textfield(class = "col-3", "When", :input, hint = "E.g., !C, !D"),
-#             Stipple.Html.div(class = "q-pa-md", 
-#                 col([
-#                     p("Probability", style="font-weight: bold;"),
-#                     range(0.0:0.1:1.0, 
-#                         :prob_range;
-#                         snap=true,
-#                         labelalways=true,
-#                         label=true,
-#                         lazy=true,
-#                         dragrange=true,
-#                     )])
-#                 )
-#         ]),
-#         row([  
-#             col([
-#                 p(""),
-#                 button("Assume", :constraint, class = "btn btn-primary", @on("click", "process = true")),
-#                 p(""),
-#                 button("Query", :query, class = "btn btn-primary", @on("click", "process = true")),
-#             ])
-#         ])
-
-#     ]) #col
-
-# end
 
 function custom_styles()
     ["""
     <style>
         body { background-color: #f4f4f4; }
-        .facts-container { max-width: 1200px; margin: auto; background: white; padding: 20px; border-radius: 8px; box-shadow: 0 2px 4px rgba(0,0,0,0.1); }
-        .facts-header { text-align: center; color: #007bff; padding-bottom: 20px; }
-        .facts-input { margin-bottom: 20px; margin-left: 5px; margin-right: 5px; }
+        .facts-container { max-width: 1200px; background: white; padding: 5px; border-radius: 8px; box-shadow: 0 2px 4px rgba(0,0,0,0.1); }
+        .facts-header { text-align: center; color: #007bff; padding-bottom: 2px; }
+        .facts-input { max-width: 1200px;  background: white; margin-bottom: 20px; margin-left: 5px; margin-right: 5px;  margin-top: 5px}
+        .facts-text { margin-bottom: 20px; margin-left: 20px; margin-right: 5px;  margin-top: 5px; border-radius: 2px;}
         .facts-list { list-style-type: none; padding: 0; }
-        .facts-item { display: flex; align-items: center; margin-bottom: 10px; padding: 10px; border-radius: 4px; background-color: #f8f9fa; }
+        .facts-item { display: flex; align-items: center; margin-bottom: 5px; padding: 5px; border-radius: 4px; background-color: #f8f9fa; }
         .facts-item label { margin-left: 10px; flex-grow: 1; }
         .facts-item button { padding: 2px 8px; }
         .facts-filters { margin-bottom: 20px; }
         .facts-filters .btn { margin-right: 5px; }
-        .facts-footer { display: flex; justify-content: space-between; align-items: center; margin-top: 20px; }
-        .btn-focused { background-color: #007bff; color: white; }
+        .btn-delete_fact_id { margin-left: 10px; color: red; border-radius: 4px; background-color: #f8f9fa;}
         [v-cloak] { display: none; }
     </style>
     """]
@@ -118,70 +101,63 @@ end
 
 function ui()
     # Add Bootstrap CSS
-    #Stipple.Layout.add_css("https://bootswatch.com/5/lumen/bootstrap.min.css")
-    Stipple.Layout.add_css("https://bootswatch.com/5/litera/bootstrap.min.css")
+    Stipple.Layout.add_css("https://bootswatch.com/5/lumen/bootstrap.min.css")
+    #Stipple.Layout.add_css("https://bootswatch.com/5/litera/bootstrap.min.css")
     # Add custom styles
     Stipple.Layout.add_css(custom_styles)
 
     [
-        row(
-            section(class="facts-container", v__cloak=true, [
-            header(class="facts-header", [
-                # h1("FactSimply: What Do Your Beliefs Imply?", style="margin-bottom: 50px; text-decoration: underline;")
-                h1("TITLE", style="margin-bottom: 50px; text-decoration: underline;")
+        section(class="facts-container", v__cloak=true, [
+            row([
+                header(class="facts-header", [
+                    h1("FactSimply: What Do Your Beliefs Imply?", style="margin-bottom: 50px; text-decoration: underline;")
                 ]),        
-        ])),
+            ])
+        ]),
         
-        section(class="facts-input", [
+        section(class="facts-container facts-input", [
             row([
             # Input field for new facts, bound to new_facts variable
-                input(class="facts-input col-2", placeholder="The joint event (e.g., 'A, !B')", @bind(:what_text)),
+                input(class="facts-text col-3", placeholder="The joint event (e.g., 'A, !B')", @bind(:what_text)),
                 cell(class="col-1", p("conditioned on", style="text-align: center; font-weight: bold;")),
-                input(class="facts-input col-2", placeholder="the joint conditions (e.g., '!A, !C')", @bind(:when_text)),
+                input(class="facts-text col-3", placeholder="the joint conditions (e.g., '!A, !C')", @bind(:when_text)),
                 p(class="col-2", " has probability in the range: ", style="font-weight: bold;"),
             
-                cell(class="col-3",   range(0.0:0.1:1.0, 
+                cell(class="col-2",   range(0.0:0.1:1.0, 
                             :prob_range;
                             snap=true,
                             labelalways=true,
                             label=true,
                             dragrange=true,
                             dense=true,
-                            thumb__color="purple",
+                            thumb__color="blue-13",
                             thumb__size="35px",
+                            switch__label__side = true,
+                            switch__marker__labels__side = true,
                             labelvalueleft=Symbol("'Min = ' + prob_range.min"),
                             labelvalueright=Symbol("'Max = ' + prob_range.max"),
-                            @on("change", "prob_range_changed = true")
-                        )),
-                        p("P({{what_text}} | {{when_text}}) ∈ [{{prob_range.min}}, {{prob_range.max}}]", style="font-weight: bold; margin-left: 10px;"),
+                         )),
+
+                        p("{{fact.printable}}", style="margin-left: 10px; font-weight: bold;"),
+
+ 
                 ]),
                 
                 
                 row([
-                    cell(class="col-1",button("Assume", class="btn btn-outline-primary", (class!)="{ 'btn-focused' : filterAll }", @on("click", "type = 'assume'"))),
-                    cell(class="col-1", button("Query", class="btn btn-outline-primary", (class!)="{ 'btn-focused' : filterActive }", @on("click", "type = 'query'"))),
+                    cell(class="col-1 btn-fact",button("Assume", class="btn btn-outline-primary", (class!)="{ 'btn-focused' : filterAll }", @click(:assume_btn_clicked ))),
+                    cell(class="col-1 btn-fact", button("Query", class="btn btn-outline-primary", (class!)="{ 'btn-focused' : filterActive }", @click(:query_btn_clicked ))),
                     ]),                   
             ]),
-
-        
-            
+          
             section(class="facts-list", [
                 ul(class="facts-list", [
-                    li(class="facts-item", @recur("facts in filtered_facts"), (key!)="facts.id", [
-                        # Checkbox for toggling facts status
-                        input(type="checkbox", class="form-check-input", @on("change", "toggle_facts = facts.id"), (checked!)="facts.completed", (id!)="'facts-' + facts.id"),
-                        # facts text
-                        label(class="form-check-label", "{{ facts.text }}", (for!)="'facts-' + facts.id"),
-                        # Delete button
-                        button("×", class="btn btn-sm btn-outline-danger", @on("click", "delete_facts = facts.id"))
+                    li(class="facts-item", @recur("fact in facts"), [
+                        p("{{fact.printable}}", style="margin: 0;"),
+                        button("×", class="btn-delete_fact_id", outline=true,color = "red", @on("click", "delete_fact_id = fact.id"))
                     ])
                 ])
             ]),
-
-            footer(class="facts-footer", [
-                # Display count of active factss
-                span("{{ active_facts }} facts left", class="text-muted")
-            ])
         ]
     
 end
--- lib/parser.jl
+++ lib/parser.jl
@@ -1,2 +1,52 @@
 
-function parse(expression)end
+
+
+
+function parse_input(expression)
+    # Split the input by commas and remove leading/trailing spaces
+    substrings = split(expression, ",")
+    print_strings = []
+    var_symbols = Symbol[]
+    vals = Bool[]
+
+    for substring in substrings
+        cleaned_str = strip(substring)
+        is_negated = startswith(cleaned_str, "!")
+
+        if is_negated
+            cleaned_str = cleaned_str[2:end]
+        end
+
+        length(cleaned_str) == 0 && continue
+
+        cleaned_str = uppercase(cleaned_str)
+        value = is_negated ? false : true
+        push!(var_symbols, Symbol(cleaned_str))
+        push!(vals, value)
+        push!(print_strings, "$cleaned_str=$value")
+    end
+
+    return (; printable = join(print_strings, ", "), 
+            var_symbols = var_symbols, vals = vals)
+end
+
+function to_string(fact)
+    joint = parse_input(fact.what_text)
+    cond = parse_input(fact.when_text)
+    
+    prob_statement = length(cond.vals) == 0 ? "P($(joint.printable))" : "P($(joint.printable) | $(cond.printable))"
+    rhs = " = ?"
+    if !fact.is_query
+        if fact.prob_range.max - fact.prob_range.min < 0.01
+            rhs = " = $(fact.prob_range.min)"
+        else
+            rhs = " ∈ [$(fact.prob_range.min), $(fact.prob_range.max)]"
+        end
+    end
+
+    return  prob_statement * rhs
+end
+
+
+
+export parse_input, to_string
