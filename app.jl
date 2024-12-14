02f8755bb8b5bf87d9098cefd941080d10c86628
Harsha Veeramachaneni <hveera@gmail.com>
2024-12-14 13:39:58 -0600

    styles and add clear buttn

--- app.jl
+++ app.jl
@@ -23,14 +23,33 @@ function Fact(id, what_text, when_text, prob_range, is_query)
     return f
 end
 
+# function Base.convert(
+#       ::Type{RangeData{Float64}},
+#       r::@NamedTuple{min::Float64, max::Float64},
+#   ) 
+#   @show r
+#     return RangeData(UnitRange(r.min, r.max))
+# end
+
+# function Base.convert(
+#     ::@NamedTuple{min::Float64, max::Float64},
+#     r::Type{RangeData{Float64}}
+# ) 
+#     return (min=first(r.range), max=last(r.range))
+# end
+
 @app begin
     @private id = 1
     @in what_text = ""
     @in when_text = ""
-    @in prob_range = (min=0.0, max=1.0)
+    @in range_slider_val =  RangeData(0:20)
+    
     @in assume_btn_clicked = false
     @in query_btn_clicked = false
+    @in clear_btn_clicked = false
     @in is_query = false
+
+    @out feedback_str = ""
 
     @in process_new_fact = false  
     @in clear_completed = false  
@@ -38,28 +57,41 @@ end
  
     # Define private and output variables
     @out fact = Fact(0, "", "", (min=0.0, max=1.0), false, "")
+    @private prob_range = (min=0.0, max=1.0)
     @out facts = Fact[]  
     @out num_facts = 0  
 
-    @onbutton assume_btn_clicked  begin
-        is_query = false
-        length(parse_input(what_text).vals) == 0 && return 
-
+    @onchange what_text begin
         fact = Fact(id, what_text, when_text, prob_range, is_query)
+        feedback_str = fact.printable
+    end 
+    @onchange when_text begin
+        fact = Fact(id, what_text, when_text, prob_range, is_query)
+        feedback_str = fact.printable
+    end
+
+    @onbutton assume_btn_clicked  begin
+        @show "assume button clicked"
+        is_query = false
+        fact = Fact(id, what_text, when_text, prob_range, is_query)
+        feedback_str = fact.printable
+
+        length(parse_input(what_text).vals) == 0 && return # nothing in the text field
+
         id += 1
         push!(facts, fact)
         @push facts
 
         num_facts = length(facts)
         @show num_facts
-        for f in facts
-            @show f.what_text, f.when_text ,  f.prob_range , f.is_query
-        end
+
     end
 
     @onbutton query_btn_clicked  begin
+        @show "assume button clicked"
         is_query = true
         fact = Fact(0, what_text, when_text, (min=0.0, max=1.0), is_query)
+        feedback_str = fact.printable
     end
 
     @onchange delete_fact_id begin
@@ -68,13 +100,35 @@ end
         num_facts = length(facts)
     end
 
-    @onchange prob_range begin
-        @info "Probability range changed" prob_range
+
+    @onchange range_slider_val begin
+        @info " range changed" range_slider_val
+        prob_range = (min=first(range_slider_val.range)/100.0, max=last(range_slider_val.range)/100.0)
+        fact.prob_range = prob_range
+        feedback_str = to_string(fact)
+        @show prob_range
     end
 
+    @onbutton clear_btn_clicked  begin
+        id = 1
+        what_text = ""
+        when_text = ""
+        range_slider_val =  RangeData(0:20)
+        assume_btn_clicked = false
+        query_btn_clicked = false
+        is_query = false
 
-    @onchange facts begin
-        @info "Facts changed" facts
+        feedback_str = ""
+
+        process_new_fact = false  
+        clear_completed = false  
+        delete_fact_id = 0  
+ 
+        fact = Fact(0, "", "", (min=0.0, max=1.0), false, "")
+        prob_range = (min=0.0, max=1.0)
+        facts = Fact[]  
+        num_facts = 0
+
     end
 end
 
@@ -83,17 +137,20 @@ function custom_styles()
     ["""
     <style>
         body { background-color: #f4f4f4; }
-        .facts-container { max-width: 1200px; background: white; padding: 5px; border-radius: 8px; box-shadow: 0 2px 4px rgba(0,0,0,0.1); }
+        .facts-container {background: white; padding: 5px; border-radius: 8px; box-shadow: 0 2px 4px rgba(0,0,0,0.1); margin-bottom: 20px; }
+        .facts-header-container { background: white; padding: 5px; border-radius: 8px; box-shadow: 0 2px 4px rgba(0,0,0,0.1); }
         .facts-header { text-align: center; color: #007bff; padding-bottom: 2px; }
-        .facts-input { max-width: 1200px;  background: white; margin-bottom: 20px; margin-left: 5px; margin-right: 5px;  margin-top: 5px}
-        .facts-text { margin-bottom: 20px; margin-left: 20px; margin-right: 5px;  margin-top: 5px; border-radius: 2px;}
-        .facts-list { list-style-type: none; padding: 0; }
-        .facts-item { display: flex; align-items: center; margin-bottom: 5px; padding: 5px; border-radius: 4px; background-color: #f8f9fa; }
+        .facts-input {background: white; padding: 5px; border-radius: 8px; box-shadow: 0 2px 4px rgba(0,0,0,0.1); margin-top: 20px;}
+        .facts-text { padding: 5px; margin-bottom: 20px; margin-left: 20px; margin-right: 5px;  margin-top: 5px; }
+        .facts-list { list-style-type: none; padding: 5px; }
+        .facts-list-container {background: white; padding: 5px; border-radius: 8px; box-shadow: 0 2px 4px rgba(0,0,0,0.1); margin-left: 10px; margin-right: auto; }
+        .facts-latex-container {background: white; padding: 5px; border-radius: 8px; box-shadow: 0 2px 4px rgba(0,0,0,0.1);margin-left: auto; margin-right: 10px;}
+        .facts-item { display: flex; align-items: center; margin-bottom: 0px; padding: 5px; border-radius: 4px; background-color: #f8f9fa; }
         .facts-item label { margin-left: 10px; flex-grow: 1; }
         .facts-item button { padding: 2px 8px; }
         .facts-filters { margin-bottom: 20px; }
         .facts-filters .btn { margin-right: 5px; }
-        .btn-delete_fact_id { margin-left: 10px; color: red; border-radius: 4px; background-color: #f8f9fa;}
+        .btn-delete_fact_id { margin-left: auto; margin-right: 0; color: red; border-radius: 8px; background-color: #f8f9fa;}
         [v-cloak] { display: none; }
     </style>
     """]
@@ -107,10 +164,10 @@ function ui()
     Stipple.Layout.add_css(custom_styles)
 
     [
-        section(class="facts-container", v__cloak=true, [
+        section(class="facts-header-container", v__cloak=true, [
             row([
                 header(class="facts-header", [
-                    h1("FactSimply: What Do Your Beliefs Imply?", style="margin-bottom: 50px; text-decoration: underline;")
+                    h1("FactSimply: What Do Your Beliefs Imply?", style="margin-bottom: 20px; text-decoration: underline;")
                 ]),        
             ])
         ]),
@@ -123,40 +180,49 @@ function ui()
                 input(class="facts-text col-3", placeholder="the joint conditions (e.g., '!A, !C')", @bind(:when_text)),
                 p(class="col-2", " has probability in the range: ", style="font-weight: bold;"),
             
-                cell(class="col-2",   range(0.0:0.1:1.0, 
-                            :prob_range;
+                cell(class="col-2",   range(0:5:100, 
+                            :range_slider_val;
                             snap=true,
                             labelalways=true,
                             label=true,
                             dragrange=true,
                             dense=true,
                             thumb__color="blue-13",
-                            thumb__size="35px",
+                            thumb__size="55px",
                             switch__label__side = true,
                             switch__marker__labels__side = true,
-                            labelvalueleft=Symbol("'Min = ' + prob_range.min"),
-                            labelvalueright=Symbol("'Max = ' + prob_range.max"),
+                            labelvalueleft=Symbol("'Min = ' + range_slider_val.min/100.0"),
+                            labelvalueright=Symbol("'Max = ' + range_slider_val.max/100.0"),
                          )),
 
-                        p("{{fact.printable}}", style="margin-left: 10px; font-weight: bold;"),
-
- 
+                        p("{{feedback_str}}", style="margin-left: 10px; font-weight: bold;"),
                 ]),
-                
                 
                 row([
                     cell(class="col-1 btn-fact",button("Assume", class="btn btn-outline-primary", (class!)="{ 'btn-focused' : filterAll }", @click(:assume_btn_clicked ))),
                     cell(class="col-1 btn-fact", button("Query", class="btn btn-outline-primary", (class!)="{ 'btn-focused' : filterActive }", @click(:query_btn_clicked ))),
+                    cell(class="col-2 btn-fact", style="margin-right: 0; margin-left: auto;", button("Clear & Restart", class="btn btn-outline-primary", (class!)="{ 'btn-focused' : filterActive }", @click(:clear_btn_clicked ))),
                     ]),                   
             ]),
           
-            section(class="facts-list", [
-                ul(class="facts-list", [
-                    li(class="facts-item", @recur("fact in facts"), [
-                        p("{{fact.printable}}", style="margin: 0;"),
-                        button("×", class="btn-delete_fact_id", outline=true,color = "red", @on("click", "delete_fact_id = fact.id"))
+            row([
+                section(class="col facts-list-container", [
+                    h6("Facts", style="margin-bottom: 10px; text-decoration: underline;"),
+                    ul(class="facts-list", [
+                        li(class="facts-item", @recur("fact in facts"), [
+                            p("{{fact.printable}}", style="margin: 0;"),
+                            button("×", class="btn-delete_fact_id", outline=true,color = "red", @on("click", "delete_fact_id = fact.id"))
+                        ])
                     ])
-                ])
+                ]),
+                section(class="col facts-latex-container", [
+                    ul(class="facts-list", [
+                        li(class="facts-item", @recur("fact in facts"), [
+                            p("{{fact.printable}}", style="margin: 0;"),
+                            button("×", class="btn-delete_fact_id", outline=true,color = "red", @on("click", "delete_fact_id = fact.id"))
+                        ])
+                    ])
+                ]),
             ]),
         ]
     
